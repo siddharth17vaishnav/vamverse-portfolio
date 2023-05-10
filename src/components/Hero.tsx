2310ebd7178393ccc98ca21fcf0b6ca74bb65aa4
siddharth <siddharth17vaishnav@gmail.com>
2023-05-10 21:56:09 +0530

    make work section responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,9 +2,9 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 const Hero = (): JSX.Element => {
     return (
         <>
-            <div id="home" className=" h-[90vh] flex flex-col justify-center">
+            <div id="home" className=" h-[90vh] flex flex-col justify-center mx-auto items-center">
                 <h1 className="text-6xl mx-auto font-bold">We Make Reality</h1>
-                <h3 className="mt-5 text-2xl max-w-[52%] self-center [text-align-last:center] ">Join our hands to scale your business with the latest augment reality technologies.</h3>
+                <h3 className="mt-5 text-2xl lg:max-w-[52%] self-center [text-align-last:center] ">Join our hands to scale your business with the latest augment reality technologies.</h3>
             </div>
             <div className='w-full [text-align-last:center] mt-[-100px]'>
                 <a href="#works">
